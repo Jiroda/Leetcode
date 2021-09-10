2b2cec5049a17bf762e2bd5664f1335c96dbb3e9
Jiroda <32682465+Jiroda@users.noreply.github.com>
2021-09-09 21:23:02 -0600

    Time: 3 ms (52.65%), Space: 39 MB (43.67%) - LeetHub

--- third-maximum-number/third-maximum-number.java
+++ third-maximum-number/third-maximum-number.java
@@ -1,17 +1,39 @@
 //T:O(n)
 //S:O(1)
 
-public class Solution {
+class Solution{
     public int thirdMax(int[] nums) {
-       PriorityQueue<Integer> pq = new PriorityQueue<>();
-       Set<Integer> set = new HashSet<>();
-       for(int n : nums) {
-           if(set.add(n)) {
-               pq.offer(n);
-               if(pq.size() > 3 ) pq.poll();
-           }
-       }
-       if(pq.size() == 2) pq.poll();
-       return pq.peek();
+        
+        /*
+        Approach: Create a minHeap of size 3  with PriorityQueue. For each element, first check if the item is not
+                  already there in pq. [Note: The "contains()" method is O(n), where n is the size of pq. Since in our
+                  case the max size will be 3, it is essentially same as O(1), thus not affecting the overall O(n) time
+                  complexity of the whole program]. 
+                  If the item is not already there in pq, insert it to pq, then check if size of pq exceeds 3.
+                  If pq.size()>3 , remove first (smallest) element of pq. Thus, after we go through all the items,
+                  we'll have 3 maximum items in pq.
+                  If size of pq is 2, it means 3rd maximum doesn't exist. So poll() pq (removing the minumum item, leaving
+                  the maximum item).
+                  Now, poll pq, and return the popped value. If size of pq is less than 3, it'll have the maximum item,
+                  otherwise it'll have the 3rd maximum item, which is the requirement of this problem.
+                  
+        Complexity analysis: Time: O(n); Space: O(1), because max size of pq is 3
+        */
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(3);
+        for(int num: nums){
+            if(!pq.contains(num)){
+                pq.add(num);
+                if(pq.size()>3){
+                    pq.poll();
+                }
+            }
+        }
+        
+        if(pq.size()==2){
+            pq.poll();
+        }
+
+        return pq.poll();
     }
 }
