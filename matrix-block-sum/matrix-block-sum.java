ff96c1df4513a3a69a00c5135983689daa93df66
Jiroda <32682465+Jiroda@users.noreply.github.com>
2021-09-10 22:53:08 -0600

    Time: 9 ms (32.15%), Space: 46.8 MB (6.30%) - LeetHub

--- matrix-block-sum/matrix-block-sum.java
+++ matrix-block-sum/matrix-block-sum.java
@@ -1,28 +1,24 @@
+//T:O(m*n)
+//S:O(m*n)
+
 class Solution {
     public int[][] matrixBlockSum(int[][] mat, int K) {
         int m = mat.length, n = mat[0].length;
-        int dp[][] = new int[m+1][n+1];
-        int ans[][] = new int[m][n];
-        computePrefix(dp,mat,m,n);
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-               int r1 = Math.max(0, i - K), c1 = Math.max(0, j - K);
-               int r2 = Math.min(m - 1, i + K), c2 = Math.min(n - 1, j + K);   
-               ans[i][j] = rangeSum(dp,r1,c1,r2,c2);
+        int[][] sum = new int[m + 1][n + 1]; // sum[i][j] is sum of all elements from rectangle (0,0,i,j) as left, top, right, bottom corresponding
+        for (int r = 1; r <= m; r++) {
+            for (int c = 1; c <= n; c++) {
+                sum[r][c] = mat[r - 1][c - 1] + sum[r - 1][c] + sum[r][c - 1] - sum[r - 1][c - 1];
+            }
+        }
+        int[][] ans = new int[m][n];
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                int r1 = Math.max(0, r - K), c1 = Math.max(0, c - K);
+                int r2 = Math.min(m - 1, r + K), c2 = Math.min(n - 1, c + K);
+                r1++; c1++; r2++; c2++; // Since `sum` start with 1 so we need to increase r1, c1, r2, c2 by 1
+                ans[r][c] = sum[r2][c2] - sum[r2][c1-1] - sum[r1-1][c2] + sum[r1-1][c1-1];
             }
         }
         return ans;
-    }
-    
-    void computePrefix(int[][]dp,int[][]mat,int m, int n){
-        for(int i=1;i<=m;i++)
-            for(int j=1;j<=n;j++)
-                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + mat[i-1][j-1];
-    }
-    
-    int rangeSum(int[][]dp,int r1,int c1, int r2, int c2){
-        r1++; c1++; r2++; c2++;
-        return dp[r2][c2] - dp[r1-1][c2] - dp[r2][c1-1] + dp[r1-1][c1-1];
     }
 }
