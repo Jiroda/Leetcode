8507c6179aabfc7306ba6241d260492f289fedb4
Jiroda <32682465+Jiroda@users.noreply.github.com>
2021-09-10 22:52:08 -0600

    Time: 9 ms (32.15%), Space: 46.6 MB (6.30%) - LeetHub

--- matrix-block-sum/matrix-block-sum.java
+++ matrix-block-sum/matrix-block-sum.java
@@ -1,23 +1,28 @@
-class Solution{
+class Solution {
     public int[][] matrixBlockSum(int[][] mat, int K) {
-        if(mat == null || mat.length == 0 || mat[0].length == 0){
-            return mat;
-        }
-        int[][] res = new int[mat.length][mat[0].length];
-        for(int i = 0; i < mat.length; i++){
-            for(int j = 0; j < mat[0].length; j++){
-                res[i][j] = helper(mat, i, j, K);
+        int m = mat.length, n = mat[0].length;
+        int dp[][] = new int[m+1][n+1];
+        int ans[][] = new int[m][n];
+        computePrefix(dp,mat,m,n);
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+               int r1 = Math.max(0, i - K), c1 = Math.max(0, j - K);
+               int r2 = Math.min(m - 1, i + K), c2 = Math.min(n - 1, j + K);   
+               ans[i][j] = rangeSum(dp,r1,c1,r2,c2);
             }
         }
-        return res;
+        return ans;
     }
-    private int helper(int[][] mat, int x, int y, int K){
-        int res = 0;
-        for(int i = Math.max(0, x - K); i <= Math.min(mat.length - 1, x + K); i++){
-            for(int j = Math.max(0, y - K); j <= Math.min(mat[0].length - 1, y + K); j++){
-                res += mat[i][j];
-            }
-        }
-        return res;
+    
+    void computePrefix(int[][]dp,int[][]mat,int m, int n){
+        for(int i=1;i<=m;i++)
+            for(int j=1;j<=n;j++)
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + mat[i-1][j-1];
+    }
+    
+    int rangeSum(int[][]dp,int r1,int c1, int r2, int c2){
+        r1++; c1++; r2++; c2++;
+        return dp[r2][c2] - dp[r1-1][c2] - dp[r2][c1-1] + dp[r1-1][c1-1];
     }
 }
