892abb73525aa08b821d8ec507cb7b61452af6c2
Jiroda <32682465+Jiroda@users.noreply.github.com>
2021-09-10 21:42:25 -0600

    Time: 0 ms (100.00%), Space: 38.4 MB (14.13%) - LeetHub

--- invert-binary-tree/invert-binary-tree.java
+++ invert-binary-tree/invert-binary-tree.java
@@ -21,4 +21,19 @@ class Solution {
         
         return root;
     }
+    
+    public TreeNode invertTreeBFS(TreeNode root) {
+        if (root == null) return null;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            TreeNode temp = current.left;
+            current.left = current.right;
+            current.right = temp;
+            if (current.left != null) queue.add(current.left);
+            if (current.right != null) queue.add(current.right);
+        }
+        return root;
+    }
 }
