2443e93fbfccb7f86a63648afa383ac35476accb
Jiroda <32682465+Jiroda@users.noreply.github.com>
2021-09-17 22:56:25 -0600

    Time: 50 ms (66.60%), Space: 41.9 MB (5.57%) - LeetHub

--- longest-increasing-subsequence/longest-increasing-subsequence.java
+++ longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,40 +1,73 @@
-class Solution {      
+class Solution {
     public int lengthOfLIS(int[] nums) {
-		if(nums.length == 0 || nums.length == 1) return nums.length;
-		
-		int parent[] = new int[nums.length]; // Tracking the predecessors/parents of elements of each subsequence.
-		int dp[] = new int[nums.length + 1]; // Tracking ends of each increasing subsequence.
-		int maxLength = 0;
-        int maxIdx = -1; // Length of longest subsequence.
+/*
+        Could you improve it to O(n log(n)) time complexity?
 
-		//Initialize
-		Arrays.fill(dp, 1);
-		Arrays.fill(parent, -1);
-
-		for (int i = 1; i < nums.length; i++) {
-			for (int j = 0; j < i; j++) {
-				if (nums[i] > nums[j] && dp[i] < dp[j] + 1) {
-					dp[i] = dp[j] + 1;
-					parent[i] = j;				
-				}
-			}
-			if (dp[i] > maxLength) {
-				maxLength = dp[i];
-				maxIdx = i;
-			}
-		}
-
-		// Generate LIS by traversing parent array
-		int LIS[] = new int[maxLength];
-		for (int i = 0; i < maxLength; i++) {
-			LIS[i] = nums[maxIdx];
-			maxIdx = parent[maxIdx];
-		}
-
-		for (int i = 0; i < maxLength; i++) {
-			System.out.print(LIS[i] + ",");
-		}
-		return maxLength;
-	}
-    
+        Yes, using binary search. This method will not tell us actual LIS but its length only.
+        Binary Search is used as we get sorted array and we can find ceil.
+*/
+        if(nums.length == 1)
+        {
+            return 1;
+        }
+        
+        int [] dp = new int [nums.length];
+        
+        dp[0] = nums[0]; // as 1 length LIS is atleast possible
+        int ans = 1; // storing the length of current LIS
+        
+        for(int i=1;i<nums.length;i++)
+        {
+            // if current val > stored, LIS can be formed and new value added
+            if(nums[i]>dp[ans-1])
+            {
+                dp[ans]=nums[i];
+                ans++;
+            }
+            
+            else if (nums[i]==dp[ans-1]){
+                continue;
+            }
+            
+        else{
+            
+            // now if current val < stored means improvement possible
+            // we search the ceil and replace with it 
+            // ceil using binary search
+        
+            int lo = 0;
+            int hi = ans -1;
+            int temp =0;
+            
+            while(lo<hi)
+            {
+                int mid = lo + (hi - lo)/2;
+                
+                if(dp[mid]<nums[i])
+                {
+                    // ceil will be found on right side
+                        lo = mid+1;
+                } 
+               
+                else{
+                    // ceil will be found on left
+                    hi = mid;
+                }
+                
+            }
+        
+            dp[lo] = nums[i];    
+  
+        }
+            
+        }
+        
+        // it might print right answer
+        for(int i=0;i<ans;i++)
+        {
+            System.out.print(dp[i] + ",");
+        }
+        
+        return ans;
+    }
 }
