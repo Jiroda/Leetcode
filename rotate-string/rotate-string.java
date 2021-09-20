2c83f29429278cb7a0e9125f45d1c411a62876ac
Jiroda <32682465+Jiroda@users.noreply.github.com>
2021-09-19 21:23:07 -0600

    Time: 0 ms (100.00%), Space: 38.6 MB (27.82%) - LeetHub

--- rotate-string/rotate-string.java
+++ rotate-string/rotate-string.java
@@ -1,12 +1,23 @@
+// T: O(n)
+// S: O(1)
+
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(goal.length() < s.length()){
-            return false;
+        // get the first character from the target string
+        char firstLetterOfGoal = goal.charAt(0);
+        // length of the given string
+        int len = s.length();
+        // iterate over given string
+        for (int i = 0; i < len; i++) {
+            if (s.charAt(i) != firstLetterOfGoal) continue;
+            // shift the left most character with right most
+            var suffix = s.substring(i);
+            var prefix = s.substring(0, i);
+            // compare string after shift
+            if (goal.equals(suffix + prefix)) return true;
         }
-        String newString = s+s;
-        if(newString.contains(goal)){
-            return true;
-        }
+        // strings will never match
         return false;
+        
     }
 }
