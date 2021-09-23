c5462550d5adfd00a18cdee7435d3ebd3257091e
Jiroda <32682465+Jiroda@users.noreply.github.com>
2021-09-22 21:28:04 -0600

    Time: 1 ms (58.80%), Space: 38.8 MB (65.91%) - LeetHub

--- verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
+++ verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
@@ -1,3 +1,6 @@
+//T:O(n)
+//S:O(1) or O(26)
+
 class Solution {
     public boolean isAlienSorted(String[] words, String order) {
         Map<Character, Integer> map = new HashMap<>();
@@ -12,9 +15,9 @@ class Solution {
     
     private boolean isInSortedOrder(String s1, String s2, Map<Character, Integer> map) {
         int l1 = s1.length(), l2 = s2.length();
-        for (int i = 0, j = 0; i < l1 && j < l2; i++, j++) {
-            if (s1.charAt(i) != s2.charAt(j)) {
-                if (map.get(s1.charAt(i)) > map.get(s2.charAt(j))) {
+        for (int i = 0; i<Math.min(l1,l2); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                if (map.get(s1.charAt(i)) > map.get(s2.charAt(i))) {
                     return false;
                 } else {
                     return true;
