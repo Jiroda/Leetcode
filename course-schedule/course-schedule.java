20b718fd47c5aafff6c03d83d251ccdb8e8f7eb7
Jiroda <32682465+Jiroda@users.noreply.github.com>
2021-09-25 22:56:59 -0600

    Time: 4 ms (74.69%), Space: 39.5 MB (79.55%) - LeetHub

---
+++ course-schedule/course-schedule.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        int[] indegree = new int[numCourses];
+        List<Integer> courseOrder = new ArrayList<>();
+        for(int i=0; i<numCourses; i++){
+            map.put(i, new ArrayList<>());
+        }
+        for(int[] prereq : prerequisites){
+            int to = prereq[0];
+            int from = prereq[1];
+            map.get(from).add(to);
+            indegree[to]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i]==0){
+                q.offer(i);
+            }
+        }
+        
+        if(q.size()==0){
+            return false; //graph has a cycle
+        }
+        
+        while(!q.isEmpty()){
+            int course = q.poll();
+            courseOrder.add(course);
+            List<Integer> dependentCourses = map.get(course);
+            for(int dependantCourse: dependentCourses){
+                indegree[dependantCourse]--;
+                if(indegree[dependantCourse]==0){
+                    q.offer(dependantCourse);
+                }
+            }
+        }
+        
+        if(courseOrder.size()==numCourses){
+            return true;
+        }
+        
+        return false;
+        
+    }
+}
