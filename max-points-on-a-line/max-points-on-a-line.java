f7b3127203a24a313eca17257783752d14729273
Jiroda <32682465+Jiroda@users.noreply.github.com>
2021-10-02 23:34:45 -0600

    Time: 6 ms (87.60%), Space: 38 MB (93.75%) - LeetHub

---
+++ max-points-on-a-line/max-points-on-a-line.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maxPoints(int[][] points) {
+        List<Point> p = new ArrayList<>();
+        for (int[] point : points) {
+            p.add(new Point(point[0], point[1]));
+        }
+        
+        Map<Double, Integer> slopes;
+        int max = 1; // the array will have at least one point, so min is one
+        
+        for (int i = 0; i < p.size(); i++) {
+            slopes = new HashMap<>();
+            for (int j = i+1; j < p.size(); j++) {
+                Double slope = p.get(i).slope(p.get(j));
+                if (!slopes.containsKey(slope)) slopes.put(slope, 1); // set to one as we want to consider this point as well
+                
+                int val = slopes.get(slope);
+                max = Math.max(max, val+1);
+                slopes.put(slope, val+1);
+            }
+        }
+        
+        return max;
+    }
+    
+    class Point {
+        double x, y;
+        Point(int x, int y) {
+            this.x  = x;
+            this.y  = y;
+        }
+        
+        double slope(Point p) {
+            if (p.y - y == 0) return 0; // edge case
+            if (p.x - x == 0) return Double.POSITIVE_INFINITY; // edge case
+            return (p.y - y)/(p.x - x);
+        }
+    }
+}
