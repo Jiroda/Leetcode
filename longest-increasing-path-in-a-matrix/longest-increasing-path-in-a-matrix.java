e6e63cac30b5859b6bd350d19a62be608cf63d19
Jiroda <32682465+Jiroda@users.noreply.github.com>
2021-10-04 23:17:29 -0600

    Time: 9 ms (54.77%), Space: 39.8 MB (41.47%) - LeetHub

--- longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -4,39 +4,53 @@ class Solution {
     public int longestIncreasingPath(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
+        
         int result = 0;
         int[][] cache = new int[m][n];
+        boolean[][] visited = new boolean[m][n];
+        
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                result = Math.max(result, dfs(matrix, i, j, cache));
+                result = Math.max(result, dfs(matrix, i, j, cache, visited));
             }
         }
         return result;
     }
     
-    private int dfs(int[][] matrix, int row, int col, int[][] cache){
+    private int dfs(int[][] matrix, int row, int col, int[][] cache, boolean[][] visited){
         //check cache
         if(cache[row][col]>0){
             return cache[row][col];
         }
+        if(visited[row][col]){
+            return 0;
+        }
+        
+        visited[row][col] = true;
         
         int currentMaxLength =0; 
+        
         //Check all 4 directions from current coordinate
         for(int[] dir: dirs){
             int newRow = row +dir[0];
             int newCol = col +dir[1];
             if(isValid(matrix, row, col, newRow, newCol)){
-                int pathLength = dfs(matrix, newRow, newCol, cache);
+                int pathLength = dfs(matrix, newRow, newCol, cache, visited);
                 currentMaxLength = Math.max(currentMaxLength, pathLength);
             }
         }
+        
         cache[row][col] = 1+ currentMaxLength;
+        
+        visited[row][col] = false;
         
         return cache[row][col];
     }
     
     private boolean isValid(int[][] matrix, int row, int col, int newRow, int newCol){
-        if(newRow<0 || newRow >=matrix.length || newCol<0 || newCol >= matrix[0].length || matrix[newRow][newCol] <= matrix[row][col]){
+        if(newRow<0 || newRow >=matrix.length || newCol<0 || newCol >= matrix[0].length || 
+           matrix[newRow][newCol] <= matrix[row][col])
+        {
             return false;
         }
         return true;
