1d0ced833f237a941c4369743931423972a14c46
Jiroda <32682465+Jiroda@users.noreply.github.com>
2021-10-23 21:30:23 -0600

    Time: 0 ms (100.00%), Space: 55.4 MB (5.27%) - LeetHub

--- missing-element-in-sorted-array/missing-element-in-sorted-array.java
+++ missing-element-in-sorted-array/missing-element-in-sorted-array.java
@@ -1,18 +1,22 @@
 class Solution {
     public int missingElement(int[] nums, int k) {
-        int diff = 0;
-        for(int i=0 ;i<nums.length-1; i++){
-            int j = i+1;
-            diff = nums[j]-nums[i]-1;
-            if(diff==0){
-                continue;
-            }
-            if(k <= diff){
-                return nums[i]+k;
+        //binary search
+        int left =0;
+        int right = nums.length-1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            int missingElements = findMissingNumbersTillCurrentIndex(nums, mid);
+            if(missingElements<k){
+                left = mid+1;
             }else{
-                k = k-diff;
+                right = mid-1;
             }
         }
-        return k>0 ? nums[nums.length-1]+k : -1;
+        
+        return nums[left-1] + k -findMissingNumbersTillCurrentIndex(nums, left-1);
+    }
+    
+    private int findMissingNumbersTillCurrentIndex(int[] nums, int index){
+        return nums[index] - nums[0] - index;
     }
 }
