c054c1a2faf096ef5dd5eaeb21a993f3d746b441
Jiroda <32682465+Jiroda@users.noreply.github.com>
2021-10-25 12:00:18 -0600

    Time: 14 ms (22.69%), Space: 51.6 MB (42.73%) - LeetHub

--- valid-palindrome-ii/valid-palindrome-ii.java
+++ valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,3 +1,6 @@
+//T:O(n)
+//S:O(1)
+
 class Solution {
     public boolean validPalindrome(String s) {
         int left = 0;
@@ -17,6 +20,7 @@ class Solution {
         return true;
     }
     
+    //Generic helper method to extend the problem to more than one allowed deletion if possible
     private boolean isValidPalindrome(String s, int left, int right, int numOfDeletionsPossible){
         while(left<right){
             if(s.charAt(left)!=s.charAt(right)){
