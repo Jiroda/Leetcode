76a3737d4f05681e53a182644787367317bf12d5
Jiroda <32682465+Jiroda@users.noreply.github.com>
2021-11-10 20:49:58 -0700

    Time: 0 ms (100.00%), Space: 37.5 MB (53.54%) - LeetHub

--- spiral-matrix/spiral-matrix.java
+++ spiral-matrix/spiral-matrix.java
@@ -1,38 +1,34 @@
-//T:O(M * N )
-//S:O(1) in place
-
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> result = new ArrayList<>();
-        if(matrix==null || matrix.length ==0 || matrix[0].length==0){
-            return result;
-        }
         int m = matrix.length;
         int n = matrix[0].length;
+        List<Integer> res = new ArrayList<>();
+        
         int capacity = m*n;
         int top = 0;
-        int right = matrix[0].length-1;
         int left = 0;
-        int bottom = matrix.length-1;
+        int right = n-1;
+        int bottom = m-1;
         
-        while(result.size() < capacity){
-            for(int i=top, j=left; j<=right && result.size()<capacity; j++){
-                result.add(matrix[i][j]);
+        while(res.size()< capacity){
+            for(int i=top, j=left; res.size()<capacity && j<=right; j++){
+                res.add(matrix[i][j]);
             }
             top++;
-            for(int i=top, j=right; i<=bottom && result.size()<capacity; i++){
-                result.add(matrix[i][j]);
+            for(int i=top, j=right; res.size()<capacity && i<=bottom; i++){
+                res.add(matrix[i][j]);
             }
             right--;
-            for(int i=bottom, j=right; j>=left && result.size()<capacity; j--){
-                result.add(matrix[i][j]);
+            for(int i=bottom, j=right; res.size()<capacity && j>=left; j--){
+                res.add(matrix[i][j]);
             }
             bottom--;
-            for(int i=bottom, j=left; i>=top && result.size()<capacity; i--){
-                result.add(matrix[i][j]);
+            for(int i=bottom, j=left; res.size()<capacity && i>=top; i--){
+                res.add(matrix[i][j]);
             }
             left++;
         }
-        return result;
+        
+        return res;
     }
 }
