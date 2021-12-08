d8c02556ac0665605c8ff4a6e1944941a90deb5a
Jiroda <32682465+Jiroda@users.noreply.github.com>
2021-12-07 22:11:21 -0700

    Time: 2 ms (19.22%), Space: 39.3 MB (35.45%) - LeetHub

--- binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -1,31 +1,45 @@
-//T:O(n)
-//S:O(n)
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        if(root==null){
+            return new ArrayList<>();
+        }
         List<List<Integer>> result = new ArrayList<>();
-        if(root==null) return result;
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-        int level = 0;
+        int level =0;
         while(!queue.isEmpty()){
             int size = queue.size();
-            List<Integer> levelVals = new ArrayList<>();
+            List<Integer> levelValues = new ArrayList<>();
             for(int i=0; i<size; i++){
-                TreeNode node = queue.poll();
+                TreeNode curr = queue.poll();
                 if(level%2==1){
-                    levelVals.add(0, node.val);
+                    levelValues.add(0, curr.val);
                 }else{
-                    levelVals.add(node.val);
+                    levelValues.add(curr.val);
                 }
-                if(node.left!=null){
-                    queue.offer(node.left);
+                if(curr.left!=null){
+                    queue.offer(curr.left);
                 }
-                if(node.right!=null){
-                    queue.offer(node.right);
+                if(curr.right!=null){
+                    queue.offer(curr.right);
                 }
             }
-            result.add(levelVals);
+            result.add(levelValues);
             level++;
         }
         return result;
