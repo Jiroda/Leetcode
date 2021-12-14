91e96f2797827d31355bf5479394d7244c76d95f
Jiroda <32682465+Jiroda@users.noreply.github.com>
2021-12-14 11:03:54 -0700

    Time: 0 ms (100.00%), Space: 39.7 MB (6.36%) - LeetHub

--- flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -20,15 +20,22 @@ class Solution {
     public void flatten(TreeNode curr) {
         TreeNode prev = null;
         while(curr!=null){
+            //Keep going till the left subtree becomes null
             if(curr.left!=null){
+                //hold the curr.left in a previous pointer
                 prev = curr.left;
+                //move down the left subtree of the previous ptr till you reach the tail end
                 while(prev.right!=null){
                     prev = prev.right;
                 }
+                //link the left tail and the right subtree of the current
                 prev.right = curr.right;
+                //set left subtree of current to its right subtree
                 curr.right = curr.left;
+                //set currents left to null
                 curr.left = null;
             }
+            //move to the next child in the right subtree of current till you flatten the whole tree
             curr = curr.right;
         }
     }
