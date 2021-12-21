72708f61507488dae696b1bbd0c8ae0f2a7d51d3
Jiroda <32682465+Jiroda@users.noreply.github.com>
2021-12-21 11:49:21 -0700

    Time: 1 ms (92.67%), Space: 37.2 MB (81.21%) - LeetHub

--- decode-ways/decode-ways.java
+++ decode-ways/decode-ways.java
@@ -1,29 +1,42 @@
 //T:O(n)
 //S:O(n)
+
 class Solution {
     public int numDecodings(String s) {
-        int dp[] = new int[s.length()];
-        Arrays.fill(dp, -1);
-        return numDecodings(dp, s, 0); 
+        Integer memo[] = new Integer[s.length()];
+        return numDecodings(memo, s, 0); 
     }
     
-    int numDecodings(int[] dp, String s, int pos) {
-        if(pos >= s.length())
+    int numDecodings(Integer[] memo, String s, int pos) {
+        //if we reached the end of the string or overshot it
+        if(pos >= s.length()){
             return 1;
+        }
+        
+        //if the char at curr position is 0 we return 0 ways
         if(s.charAt(pos) == '0')
             return 0;
         
-        if(dp[pos] != -1)
-            return dp[pos];
+        //does the cache already have it
+        if(memo[pos] != null)
+            return memo[pos];
         
         int result = 0;
-        // one position movement
-        result += numDecodings(dp, s, pos + 1);
-        // 2 position movement
-        if(pos + 2 <= s.length() && isValid(s.substring(pos, pos + 2)))
-            result += numDecodings(dp, s, pos + 2);
         
-        return (dp[pos] = result);
+        // one position movement
+        int oneWay = numDecodings(memo, s, pos + 1);
+        result+=oneWay;
+        
+        // 2 position movement
+        if(pos + 2 <= s.length() && isValid(s.substring(pos, pos + 2))){
+            int twoWay = numDecodings(memo, s, pos + 2);
+            result+=twoWay;
+        }
+        
+        //store result for the current position in the memo
+        memo[pos] = result;
+        
+        return memo[pos];
     }
     
     boolean isValid(String num) {
