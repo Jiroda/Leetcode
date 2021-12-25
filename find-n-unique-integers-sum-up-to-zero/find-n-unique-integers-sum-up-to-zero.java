e96ebd650243ab1a65b4ea88d06f9bb80afe455d
Jiroda <32682465+Jiroda@users.noreply.github.com>
2021-12-25 02:00:19 -0700

    Time: 0 ms (100.00%), Space: 37.2 MB (82.08%) - LeetHub

---
+++ find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.java
@@ -0,0 +1,14 @@
+//T:O(n)
+//S:O(n)
+class Solution {
+    public int[] sumZero(int n) {
+        int[] res = new int[n];
+        int left = 0, right = n - 1, start = 1;
+        while (left < right) {
+            res[left++] = start;
+            res[right--] = -start;
+            start++;
+        }
+        return res;
+    }
+}
