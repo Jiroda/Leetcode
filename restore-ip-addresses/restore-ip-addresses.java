9621818c61bbcfbba8ef44ee3091fde4cd42485e
Jiroda <32682465+Jiroda@users.noreply.github.com>
2021-12-25 21:36:28 -0700

    Time: 3 ms (66.28%), Space: 39.6 MB (21.35%) - LeetHub

--- restore-ip-addresses/restore-ip-addresses.java
+++ restore-ip-addresses/restore-ip-addresses.java
@@ -21,37 +21,35 @@ class Solution {
         
         for(int i=curr; i<s.length(); i++){
             String segment = s.substring(curr, i+1);
-            
-            //If invalid we break as there is no use proceeding further
-            if(!isValid(segment)){
-                break;
+            if(isValid(segment)){
+                //store current sb length
+                int length = sb.length();
+                //choose
+                sb.append(segment);
+                //explore
+                if(i+1==s.length()){
+                    //this is the last segment
+                    backtrack(result, s, i+1, count, sb);
+                }else{
+                    //Add a . as there can be more segments
+                    sb.append(".");
+                    backtrack(result, s, i+1, count+1, sb);
+                }
+                //unchoose
+                sb.setLength(length);
             }
-            
-            //store current sb length
-            int length = sb.length();
-            //choose
-            sb.append(segment);
-            
-            //explore
-            if(i+1==s.length()){
-                //this is the last segment
-                backtrack(result, s, i+1, count, sb);
-            }else{
-                //Add a . as there can be more segments
-                sb.append(".");
-                backtrack(result, s, i+1, count+1, sb);
-            }
-            
-            //unchoose
-            sb.setLength(length);
         }
     }
     
     private boolean isValid(String segment){
-        if(segment.length()==0 || segment.length()>1 && segment.charAt(0)=='0' || Integer.parseInt(segment)>255){
+        if(segment.length()>3){
             return false;
         }
         
-        return true;
+        if(segment.length()==0 || segment.length()>1 && segment.charAt(0)=='0'){
+            return false;
+        }
+        
+        return Integer.parseInt(segment)<=255;
     }
 }
